815239ab2f51a53d7999b4413215e3c8fcb62bb5
laurent.gilles@codegist.org <laurent.gilles@codegist.org>
2015-06-14 20:15:19 -0500

    polymer 1.0 migration

--- CGUtils.js
+++ CGUtils.js
@@ -1,51 +1,4 @@
 (function(scope){
-    var Async = Polymer.Async;
-
-    var CGPropertyObserver = function(observed, watchedProperties, changeCallback, delay, observedTemplate){
-        var observers = [];
-        var propertyDefSource = observedTemplate || observed;
-        for(var i = 0; i < watchedProperties.length;i++){
-            var prop = watchedProperties[i];
-            var isArray = Array.isArray(propertyDefSource[prop]);
-            var obs = isArray ? new ArrayObserver(observed[prop]) : new PathObserver(observed, prop);
-            obs.open(function(){
-                this.scheduleChangeNotification();
-            }.bind(this));
-            observers.push(obs);
-        }
-
-        this.observed = observed;
-        this.observers = observers;
-        this.changeCallback = changeCallback;
-        this.delay = delay || 3000;
-        this.pendingChangeNotification = null;
-    };
-    CGPropertyObserver.prototype = {
-        destroy:function(firePendingChangeNotification){
-            var hadTask = this.pendingChangeNotification != null;
-            hadTask && firePendingChangeNotification === true && this.firePendingChangeNotification();
-            this.cancelPendingChangeNotification();
-            this.observers.forEach(function(obs){
-                obs.close();
-            });
-        },
-        cancelPendingChangeNotification:function(){
-            Async.cancel(this.pendingChangeNotification);
-            this.pendingChangeNotification = null;
-        },
-        firePendingChangeNotification:function(){
-            if(this.pendingChangeNotification == null) return;
-            this.pendingChangeNotification = null;
-            this.changeCallback(this.observed);
-        },
-        scheduleChangeNotification:function(){
-            this.observed._dirty = true;
-            this.cancelPendingChangeNotification();
-            this.pendingChangeNotification = Async.run(this.firePendingChangeNotification.bind(this), this.delay);
-        }
-    };
-
-
     var isNode = function(o){
         return (
             typeof Node === "object" ? o instanceof Node :
@@ -74,9 +27,6 @@
             clone:function(deep, target, objectA, objectB){
                 return $.extend.apply(this, arguments);
             },
-            observe:function(config){
-                return new CGPropertyObserver(config.observed, config.watchedProperties, config.changeCallback, config.delay, config.observedTemplate);
-            }
         },
         Json:{
             stringify:function(object, stripDomProperties, maxLength){
